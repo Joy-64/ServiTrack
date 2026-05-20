06ff22a79f9c2536869225632466a528f10db909
Joy <joyrivero64@gmail.com>
2026-05-20 18:17:33 -0300

    Cambios 20.5

--- js/mapa.js
+++ js/mapa.js
@@ -1,3 +1,5 @@
+/*PARA QUE FUNCIONE EL MAPA*/
+
 var map = L.map('map').setView([-32.9468, -60.6393], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -7,3 +9,26 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 L.marker([-32.9468, -60.6393]).addTo(map)
     .bindPopup('Rosario')
     .openPopup();
+
+
+/*PARA EL GEOLOCALIZADOR*/
+
+var geocoder = L.Control.geocoder({
+    defaultMarkGeocode: false
+})
+    .on('markgeocode', function (e) {
+         var latlng = e.geocode.center;
+
+        L.circleMarker(latlng, {
+        radius: 40,
+        color: '#3B82F6',
+        fillColor: '#22D3EE',
+        fillOpacity: 0.75,
+        weight: 3
+    }).addTo(map)
+    .bindPopup(e.geocode.name.split(',').slice(0, 3).join(','))
+    .openPopup();
+
+    map.setView(latlng, 30);
+})
+    .addTo(map);
