56f4205f630be0362d52acca22a0704df3da0477
Agustin <barriento.agustin@gmail.com>
2026-05-25 19:32:16 -0300

    php creado

---
+++ php/conexion.php
@@ -0,0 +1,10 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "php_serv";
+$conec = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+
+
+?>
