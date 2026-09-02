25495f821de08d8e70aafac3ac8787ee1b6cfb21
Molitzmos <barriento.agustin@gmail.com>
2026-09-02 20:00:21 -0300

    corregido contacto_enviar

--- contacto_enviar.php
+++ contacto_enviar.php
@@ -4,7 +4,7 @@ $email = $_POST['email'];
 $tel = $_POST['tel'];
 $message = $_POST['message'];
 
-mysqli_connect("localhost", "root", "", "servitrack") or die("Error al conectar a la base de datos");
+$conexion = mysqli_connect("localhost", "root", "", "servitrack") or die("Error al conectar a la base de datos");
 
 
 ?>
