b6bc5c2be21d80160732de906edbc438bd9c0adf
Molitzmos <barriento.agustin@gmail.com>
2026-09-02 20:25:44 -0300

    php conectado a tabla

--- contacto_enviar.php
+++ contacto_enviar.php
@@ -5,6 +5,8 @@ $tel = $_POST['tel'];
 $message = $_POST['message'];
 
 $conexion = mysqli_connect("localhost", "root", "", "servitrack") or die("Error al conectar a la base de datos");
+$consulta = "INSERT INTO contactanos (nombre, correo_electronico, telefono, mensaje) VALUES ('$name', '$email', '$tel', '$message')";
 
-
+mysqli_query($conexion, $consulta) or die("Error al insertar los datos en la base de datos");
+echo "Mensaje enviado exitosamente."
 ?>
