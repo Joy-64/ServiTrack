87ed9defa421c16a5a3c9d5411e4b87fe41520c0
Molitzmos <barriento.agustin@gmail.com>
2026-09-02 21:13:22 -0300

    Formulario de contacto completo

---
+++ conexion.php
@@ -0,0 +1,6 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$tel = $_POST['tel'];
+$message = $_POST['message'];
+
--- contactanos.php
+++ contactanos.php
@@ -1,12 +1,8 @@
 <?php
-$name = $_POST['name'];
-$email = $_POST['email'];
-$tel = $_POST['tel'];
-$message = $_POST['message'];
+require_once 'conexion.php';
 
 $conexion = mysqli_connect("localhost", "root", "", "servitrack") or die("Error al conectar a la base de datos");
 $consulta = "INSERT INTO contactanos (nombre, correo_electronico, telefono, mensaje) VALUES ('$name', '$email', '$tel', '$message')";
 
 mysqli_query($conexion, $consulta) or die("Error al insertar los datos en la base de datos");
-echo "Mensaje enviado exitosamente."
-?>
+echo "Mensaje enviado exitosamente.";
