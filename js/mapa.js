86b51620de197c83c113ea07e540cbbd277b1b8c
facutorr2005 <facutorresisei@gmail.com>
2026-09-23 21:03:46 -0300

    Miercoles 23/9

--- js/mapa.js
+++ js/mapa.js
@@ -2,6 +2,15 @@
 
 var map = L.map('map').setView([-32.9468, -60.6393], 13);
 
+var limitesRosario = L.latLngBounds(
+    [-33.05, -60.85],
+    [-32.85, -60.55]
+);
+
+map.setMaxBounds(limitesRosario);
+map.options.maxBoundsViscosity = 1;
+map.setMinZoom(12.5);
+
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
@@ -17,18 +26,23 @@ var geocoder = L.Control.geocoder({
     defaultMarkGeocode: false
 })
     .on('markgeocode', function (e) {
-         var latlng = e.geocode.center;
+        var latlng = e.geocode.center;
 
+        if (!limitesRosario.contains(latlng)) {
+            alert("Buscá una ubicación dentro de la zona de Rosario.");
+            return;
+        }
         L.circleMarker(latlng, {
-        radius: 40,
-        color: '#3B82F6',
-        fillColor: '#22D3EE',
-        fillOpacity: 0.75,
-        weight: 3
-    }).addTo(map)
-    .bindPopup(e.geocode.name.split(',').slice(0, 3).join(','))
-    .openPopup();
+            radius: 40,
+            color: '#3B82F6',
+            fillColor: '#22D3EE',
+            fillOpacity: 0.75,
+            weight: 3
+        }).addTo(map)
+            .bindPopup(e.geocode.name.split(',').slice(0, 3).join(','))
+            .openPopup();
 
-    map.setView(latlng, 30);
-})
+        map.setView(latlng, 16);
+    })
     .addTo(map);
+
